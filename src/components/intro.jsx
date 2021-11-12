0ccd970ee90887e0b747d2f4ee4d2acf5cc08d5c
cartory <xXcartoryXx@gmail.com>
2021-11-11 21:42:21 -0400

    add section > intro

---
+++ src/components/intro.jsx
@@ -0,0 +1,33 @@
+import '../styles/intro.css'
+import logo from '../assets/logo.svg'
+
+import {
+	Email as EmailIcon
+} from '@mui/icons-material'
+
+import {
+	Button
+} from '@mui/material'
+
+export const Intro = ({ title, subtitle, description }) => {
+	return (
+		<section className="intro">
+			<img src={logo} alt="logo" />
+			<div>
+				<h1 className='title typewriter'>
+					{title[0]} <strong>{title[1]}</strong>{title[2]}
+				</h1>
+			</div>
+			<h2 className='subtitle'>{subtitle}</h2>
+			<p>{description}</p>
+			<br />
+			<Button
+				variant='outlined'
+				className='button'
+				startIcon={<EmailIcon />}
+			>
+				Say Hello
+			</Button>
+		</section>
+	)
+}
