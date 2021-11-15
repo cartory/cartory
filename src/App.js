c3de653b2208abe7b3afd35a41032bd60de9b753
cartory <xXcartoryXx@gmail.com>
2021-11-15 11:46:22 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import data from './assets/data.json'
 
 import { Intro } from './components/intro'
+import { About } from './components/about'
+
 import { FloatingInfo } from './components/info'
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className="App">
       <FloatingInfo />
       <Intro {...data.intro} />
+      <About {...data.about} />
     </div>
   );
 }
